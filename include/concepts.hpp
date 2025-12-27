14807fa835d12c52f64ccba52cd1da764dff892b
Nikolay Chernyshev <n.d.chernyshev@hotmail.com>
2025-12-27 19:21:18 +0300

    Clean-up

--- include/concepts.hpp
+++ include/concepts.hpp
@@ -11,32 +11,38 @@ namespace bookdb
     concept BookContainerLike = requires(Args&&... args)
     {
         // В сущности исполнен метод emplace_back
-        { T().emplace_back(std::forward<Args>(args)...) } ->
+        { T{}.emplace_back(std::forward<Args>(args)...) } ->
             std::same_as<Book&>;
 
         // Можно определить количество содержащихся элементов
-        T().size();
+        T{}.size();
 
         // Сущность должна поддерживать итераторы
         typename T::iterator;
         typename T::const_iterator;
 
         // В сущности должно быть возможным обращаться к началу и концу
-        { T().front() } -> std::same_as<Book&>;
-        { T().back() } -> std::same_as<Book&>;
+        { T{}.front() } -> std::same_as<Book&>;
+        { T{}.back() } -> std::same_as<Book&>;
 
         // Сущность должна обладать методами begin() и end()
-        { T().begin() } -> std::same_as<typename T::iterator>;
-        { T().cbegin() } -> std::same_as<typename T::const_iterator>;
-        { T().end() } -> std::same_as<typename T::iterator>;
-        { T().cend() } -> std::same_as<typename T::const_iterator>;
+        { T{}.begin() } -> std::same_as<typename T::iterator>;
+        { T{}.cbegin() } -> std::same_as<typename T::const_iterator>;
+        { T{}.end() } -> std::same_as<typename T::iterator>;
+        { T{}.cend() } -> std::same_as<typename T::const_iterator>;
     };
 
     template <typename It>
-    concept BookIterator = requires(It it)
+    concept BookIterator = requires
     {
-        { *it } -> std::same_as<Book&>; // Dereferencing
-        { it.operator->() } -> std::same_as<Book*>; // Using '->'
+        { *It{} } -> std::same_as<Book&>; // Dereferencing
+        { It{}.operator->() } -> std::same_as<Book*>; // Using '->'
+    };
+
+    template <typename S, typename It>
+    concept BookSentinel = BookIterator<It> && requires
+    {
+        S{}.operator==(It{});
     };
 
     template <typename P>
